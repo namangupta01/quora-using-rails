3e24daaa7fa15482cd2bc2307e016dd889d57276
namangupta01 <01namangupta@gmail.com>
2017-03-31 22:09:16 +0530

    bug in sign up system is removed

--- app/controllers/authentication_system_controller.rb
+++ app/controllers/authentication_system_controller.rb
@@ -3,17 +3,39 @@ class AuthenticationSystemController < ApplicationController
 	def signin_get		
 	end
 
+	def signup_get
+	end
+	
 	def sign_in
 		name=params[:name]
 		password=params[:password]
 		email=params[:email]
-		user=User.new
-		user.name=name
-		user.email=email
-		user.password=password
-		user.save
-		session[:user_id]=user.id
-		redirect_to '/'
+		user=User.where(:email).first
+		if user
+			if user.password==password
+			else 
+				redirect_to'/'
+			end
+		end
+	end
+
+	def sign_up
+		name=params[:name]
+		password=params[:password]
+		email=params[:email]
+		puts "i am here"
+		user=User.where(email: email).first
+		if user
+			redirect_to '/signup_get'
+		else
+			user=User.new
+			user.name=name
+			user.email=email
+			user.password=password
+			user.save
+			session[:user_id]=user.id
+			redirect_to '/'
+		end
 	end
 
 	
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'signin_get' => 'authentication_system#signin_get'
 
+  get '/signup_get' => 'authentication_system#signup_get'
+
   post 'authentication_system/sign_in'
 
   get '/logout' => 'authentication_system#logout'
@@ -13,6 +15,8 @@ Rails.application.routes.draw do
   get '/ask_question' => 'home#ask_question'
 
   get '/questions_asked' => 'home#questions_asked'
+
+  post '/sign_up'  =>  'authentication_system#sign_up'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
